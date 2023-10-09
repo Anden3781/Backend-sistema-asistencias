182dd83d45c5cc3e0ce531a59e1c0448b4b8caa1
Caleb <reateguicevallosc@gmail.com>
2023-10-09 13:32:54 -0500

    Update; API Error

--- backendv2/routes/api/admin.php
+++ backendv2/routes/api/admin.php
@@ -36,7 +36,7 @@ Route::post('justification/decline/{id}', [App\Http\Controllers\JustificationCon
 // ---------------- USERS URL´s ---------------------- //
 Route::get('users', [App\Http\Controllers\UserController::class, 'index']);
 Route::get('users/{id}', [App\Http\Controllers\UserController::class, 'show']);
-Route::get('users/{id}/update', [App\Http\Controllers\UserController::class, 'update']);
+Route::post('users/{id}/update', [App\Http\Controllers\UserController::class, 'update']);
 
 // ---------------- BIRTHDAYS URL's ---------------- //
 Route::get('/birthday/details', [\App\Http\Controllers\BirthdayController::class, 'detailsbirthdayMonth']);
