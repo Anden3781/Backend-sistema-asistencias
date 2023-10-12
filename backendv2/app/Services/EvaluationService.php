90ee945d5ac307012eb916b493a75feadb256dad
Caleb <reateguicevallosc@gmail.com>
2023-10-12 11:48:03 -0500

    Add: Role Implementation on Evaluations

--- backendv2/app/Services/EvaluationService.php
+++ backendv2/app/Services/EvaluationService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\Evaluation;
+use App\Models\ModelHasRole;
 use App\Models\Note;
 use App\Repositories\EvaluationRepositories\EvaluationRepositoryInterface;
 
@@ -26,21 +27,38 @@ class EvaluationService {
         return $this->evaluationRepository->create($data);
     }
 
-    public function storeEvaluationNotes(array $data, $id) {
+    public function storeEvaluationNotes(array $data, $id){
         // Buscar si ya existen notas para la evaluación con el ID dado
         $existingEvaluation = Evaluation::find($id);
+        $user_id = auth()->id();
 
         if (!$existingEvaluation) {
             // Ya existen notas para esta evaluación, devolver un mensaje de error
             return response()->json(['message' => 'Esta evaluacion no existe']);
         } else {
+
+            //Guardamos la información en la evaluacion
             $existingEvaluation->softskills = $data['softskills'];
             $existingEvaluation->performance = $data['performance'];
             $existingEvaluation->hardskills = $data['hardskills'];
             $existingEvaluation->autoevaluation = $data['autoevaluation'];
+
+            //Accedemos al rol del usuario logueado
+            $roleId = ModelHasRole::where('model_id', $user_id)->get('role_id');
+            $rol = $roleId[0]->role_id;
+
+            //Calculamos el promedio en base a los roles del usuario logueado
+            if ($rol == 2) {
+                $prom = $existingEvaluation->autoevaluation;
+            } elseif ($rol == 3) {
+                $prom = ($existingEvaluation->hardskills + $existingEvaluation->performance +  $existingEvaluation->softskills) / 3;
+            }
+
+            //Guardamos la informacion en base de datos
             $existingEvaluation->save();
 
-            return response()->json(['message' => 'Notas de evaluación registradas con éxito', 'data' => $existingEvaluation]);
+            //Retornamos la respuesta en formato JSON
+            return response()->json(['message' => 'Notas de evaluación registradas con éxito', 'data' => $existingEvaluation, 'prom' => $prom]);
         }   
     }
     
